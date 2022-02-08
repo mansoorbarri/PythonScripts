0f33114a121371c1e39275d38ea42cb724f43c09
mansoor-codII <96657649+mansoor-codII@users.noreply.github.com>
2022-02-08 03:11:21 +0000

    Update LinuxWifi.py

--- LinuxWifi.py
+++ LinuxWifi.py
@@ -3,6 +3,13 @@
 #importing sys module
 import sys
 
+#importing module named subprocess
+import subprocess
+
+def my_except_hook(exctype, value, traceback):
+        print('Usage: python3', args[0] + ' <SSID>')
+sys.excepthook = my_except_hook
+
 #The sys module in Python has the argv functionality. This functionality returns a list of all command-line arguments provided to the python file when triggering an execution of it through terminal.
 args = sys.argv
 
@@ -14,8 +21,5 @@ a = '''
 '''
 print(a)
 
-#importing module named subprocess
-import subprocess
-
-#running the command to show password for the given wifi ssid
+ #running the command to show password for the given wifi ssid
 subprocess.run("cd /etc/NetworkManager/system-connections/ && sudo cat " + args[1] + " | grep 'psk=' | awk '{print substr($1,5)}'" , shell=True)
