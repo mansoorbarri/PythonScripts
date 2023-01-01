65d86bf93fa25570b7a9be89598eeede5eb2639c
Mansoor Barri <90044118+mansoorbarri@users.noreply.github.com>
2023-01-01 16:32:32 +0000

    Add files via upload

---
+++ copy-files.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# import needed libraries
+import sys
+import subprocess
+
+# set variables
+args = sys.argv
+beta_dir = "~/Music/anar/hugo/beta.mansoorbarri.com/"
+base_dir = "~/Music/anar/hugo/mansoorbarri.com/"
+x=" "
+post_md = args[2] + ".md"
+
+# copy the images 
+subprocess.run("cp -r " + beta_dir + "images/" + args[1] + "/" + args[2] + x + base_dir + "images/" + args[1] + "/" + args[2], shell=True)
+
+print("\n Images copied to: " + base_dir + "images/" + args[1] + "/" + args[2])
+
+# copy .md file
+subprocess.run("cp -r " + beta_dir + "content/" + args[1] + "/" + post_md + x + base_dir + "content/" + args[1] + "/", shell=True)
+
+print("\n .md file copied to: " + base_dir + "content/" + args[1] + "/" + args[2])
+
+exit()
